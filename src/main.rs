a65640842acfb4b70e87247f7b364d8ce9e3d223
ukpabik <kelechiukp@gmail.com>
2026-06-30 18:29:48 -0400

    mdns listener and querying

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,121 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::event::{self, Event, KeyCode, KeyEventKind};
+use socket2::{Domain, Protocol, Socket, Type};
+use std::{
+    io as stdio,
+    net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket as StdUdpSocket},
+    sync::Arc,
+};
+
+use tokio::io::{self, AsyncBufReadExt, BufReader};
+
+use tokio::net::UdpSocket;
+
+const MULTICAST_ADDR: Ipv4Addr = Ipv4Addr::new(224, 0, 0, 251);
+const PORT: u16 = 5353;
+const MDNS_ADDR: SocketAddr = SocketAddr::new(IpAddr::V4(MULTICAST_ADDR), PORT);
+
+const TARGET_QNAME: &[&[u8]] = &[b"_kdrop", b"_tcp", b"_local"];
+
+fn build_mdns_query() -> Vec<u8> {
+    let mut query: Vec<u8> = Vec::new();
+
+    // mDNS header: [TID, Attribute Bits, # of questions, # of answers, # auth RRs, # additional RRs]
+    query.extend_from_slice(&[0x00, 0x00]);
+    query.extend_from_slice(&[0x00, 0x00]);
+    query.extend_from_slice(&[0x00, 0x01]);
+    query.extend_from_slice(&[0x00, 0x00]);
+    query.extend_from_slice(&[0x00, 0x00]);
+    query.extend_from_slice(&[0x00, 0x00]);
+
+    for label in TARGET_QNAME {
+        query.push(label.len() as u8);
+        query.extend_from_slice(label);
+    }
+    query.push(0x00);
+
+    // QTYPE and QCLASS
+    query.extend_from_slice(&[0x00, 0x0c]);
+    query.extend_from_slice(&[0x00, 0x01]);
+
+    query
+}
+
+async fn send_mdns_query(socket: &UdpSocket) -> stdio::Result<()> {
+    let query = build_mdns_query();
+    socket.send_to(&query, MDNS_ADDR).await?;
+    Ok(())
+}
+
+fn get_local_ip() -> stdio::Result<Ipv4Addr> {
+    let socket = StdUdpSocket::bind("0.0.0.0:0")?;
+    socket.connect("8.8.8.8:80")?;
+
+    if let std::net::SocketAddr::V4(addr) = socket.local_addr()? {
+        Ok(*addr.ip())
+    } else {
+        Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Could not determine IPv4 address",
+        ))
+    }
+}
+
+#[tokio::main]
+async fn main() -> stdio::Result<()> {
+    let raw_socket = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::UDP))?;
+    let addr: SocketAddr = format!("0.0.0.0:{}", PORT).parse().unwrap();
+    let local_ip = get_local_ip()?;
+    raw_socket.bind(&addr.into())?;
+
+    raw_socket.set_nonblocking(true)?;
+    raw_socket.set_reuse_address(true)?;
+    raw_socket.set_reuse_port(true)?;
+
+    let std_socket: StdUdpSocket = raw_socket.into();
+    let socket = Arc::new(UdpSocket::from_std(std_socket)?);
+    socket.join_multicast_v4(MULTICAST_ADDR, local_ip)?;
+
+    let listener = socket.clone();
+
+    tokio::spawn(async move {
+        let mut buffer = [0u8; 4096];
+
+        loop {
+            match listener.recv_from(&mut buffer).await {
+                Ok((len, from)) => {
+                    if len >= 12 {
+                        println!("Received {len:?} bytes from {from:?}")
+                    }
+                    // TODO: Parse packet for information.
+                }
+                _ => println!("Error receiving packet"),
+            }
+        }
+    });
+
+    println!("Type 's' and press Enter to send mDNS query.");
+    println!("Type 'q' and press Enter to quit.");
+
+    let stdin = io::stdin();
+    let mut reader = BufReader::new(stdin).lines();
+
+    while let Some(line) = reader.next_line().await? {
+        match line.trim() {
+            "s" => {
+                println!("Sending mDNS query...");
+                if let Err(e) = send_mdns_query(&socket).await {
+                    println!("Error sending query: {}", e);
+                }
+            }
+            "q" => {
+                println!("Quitting...");
+                break;
+            }
+            _ => {
+                println!("Unknown command. Use 's' to send or 'q' to quit.");
+            }
+        }
+    }
+
+    Ok(())
 }
